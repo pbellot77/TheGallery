73af4228a4288c94752026a4dfbfd610f4f4177a
Patrick <patrickbellot77@gmail.com>
2017-08-16 14:10:45 -0500

    initial commit

---
+++ The Gallery/ArtCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ArtCollectionViewCell.swift
+//  The Gallery
+//
+//  Created by Patrick Bellot on 8/16/17.
+//  Copyright © 2017 Polestar Interactive LLC. All rights reserved.
+//
+
+import UIKit
+
+class ArtCollectionViewCell: UICollectionViewCell {
+    
+}
--- The Gallery/ViewController.swift
+++ The Gallery/ViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
+  @IBOutlet weak var collectionView: UICollectionView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.collectionView.dataSource = self
+    self.collectionView.delegate = self
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
   }
 
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "artCell", for: indexPath)
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: self.collectionView.bounds.size.width - 80, height: self.collectionView.bounds.size.height - 40)
+  }
 
 }
 
